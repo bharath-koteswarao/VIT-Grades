53701ed0cad2047bb856b922c90f4f1851983048
bharath-koteswarao <v.ramakoteswararao2015@vit.ac.in>
2017-07-06 10:55:31 +0530

    Initial Commit

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+//var captcha=require(__dirname+"/../public/javascripts/CaptchaParser.js");
+var fs=require("fs");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+
 });
 
 module.exports = router;
